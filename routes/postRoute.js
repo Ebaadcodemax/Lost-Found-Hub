801df6001aa2e75a95143a08e6b985f66867054b
Ebaad <ebaadcodemax@gmail.com>
2025-10-24 21:48:20 +0530

    Cleaned multer setup, fully switched to Cloudinary upload

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -7,17 +7,7 @@ const Post = require('../models/post')
 
 const { upload } = require("../config/cloudinary");
 
-const fileFilter = (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
 
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: Only images allowed!');
-    }
-};
 
 
 
